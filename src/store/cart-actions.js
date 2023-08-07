60a1200e8f4e1cf6d112f1d00a2d5c81f610d7ba
Saber Ahmadi <saberahmadi45@gmail.com>
2023-08-07 12:43:19 -0500

    Add files via upload

---
+++ src/store/cart-actions.js
@@ -0,0 +1,86 @@
+import { uiActions } from "./ui-slice";
+import { cartActions } from "./cart-slice";
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://react-http-c5dec-default-rtdb.firebaseio.com/cart.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Could not fetch cart data!");
+      }
+
+      const data = await response.json();
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.sowNotification({
+          status: "error",
+          title: "Error...",
+          message: "Fetching cart data failed!",
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.sowNotification({
+        status: "Pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-http-c5dec-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending Cart data Failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.sowNotification({
+          status: "success",
+          title: "Success...",
+          message: "Sending cart data !",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.sowNotification({
+          status: "error",
+          title: "Error...",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
